42af9e7178e79f29bfc487bcd19fecf7d1f92d52
S.Percentage <Syn.Tri.Naga@gmail.com>
2020-01-26 14:15:29 +0900

    add: sample unity behavior script

---
+++ screen_capture/UnityAssets/NativeRenderInteceptor.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections;
+using System.Runtime.InteropServices;
+using UnityEngine;
+
+/// <summary>
+/// Copy RenderingInterceptor.dll from target/debug into Plugins/x86_64 and Attach this script to Main Camera
+/// </summary>
+public class NativeRenderInteceptor : MonoBehaviour
+{
+    [DllImport("RenderingInterceptor")]
+    private static extern IntPtr rendering_event_ptr();
+    [DllImport("RenderingInterceptor")]
+    private static extern void set_render_buffer(IntPtr buf);
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        this.StartCoroutine(this.RenderInterceptorAtFrameTail());
+    }
+
+    IEnumerator RenderInterceptorAtFrameTail()
+    {
+        while (true)
+        {
+            yield return new WaitForEndOfFrame();
+
+            set_render_buffer(Graphics.activeColorBuffer.GetNativeRenderBufferPtr());
+            GL.IssuePluginEvent(rendering_event_ptr(), 1);
+        }
+    }
+}
